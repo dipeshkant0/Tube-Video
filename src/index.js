bd1256402f955940106cc5ef4c1286c11360fbbf
Dipesh Kant <dipeshkant0@gmail.com>
2024-02-20 17:10:03 +0530

    Database connection and Utils

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+
+import connectDB from './db/dbconnect.js'
+import {app} from "./app.js"
+
+dotenv.config(
+  {
+     path:'./env'
+  }
+)
+
+
+connectDB()
+.then(
+     ()=>{
+          app.on('error',(error)=>{
+               console.log("ERROR:",error);
+          })
+          app.listen(process.env.PORT||8000,()=>{
+               console.log(`Server is running on port ${process.env.PORT||8000}`);
+          })
+     }
+     
+)
+.catch(
+     err =>{
+          console.log("MongoDB connection Failed")
+     }
+)
